14d1631f2d7f688dea5ec6065468590d745506d6
Drew Hund <languageinvestigator@gmail.com>
2026-04-11 15:40:02 -0400

    UI done

--- main.py
+++ main.py
@@ -20,10 +20,23 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "italic"))
+timer_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 photo = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=photo)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+checkmarks = Label(text="✓", bg=YELLOW, fg=GREEN)
+checkmarks.grid(column=1, row=3)
 
 window.mainloop()
